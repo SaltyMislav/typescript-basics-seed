db7cae33a7be41ca8c5c0cd0ecc8873429a3fa74
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-14 15:22:59 +0200

    video 36

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,10 @@
-interface SizesInterface {
-    availableSizes: string[];
-}
-
-abstract class Sizes implements SizesInterface {
-    constructor (protected sizes: string[]) {}
-
-    set availableSizes (sizes: string[]) {
-        this.sizes = sizes;
-    }
-
-    get availableSizes () {
-        return this.sizes;
+class Cupon {
+    static allowed = ['Pepperoni', 'Blazing Inferno'];
+    static create(percentage: number) {
+        return `PIZZA_RESTAURANT_${percentage}`;
     }
 }
 
-interface PizzaInterface extends SizesInterface {
-    readonly name: string;
-    toppings: string[];
-    updateSizes (sizes: string[]): void;
-    addTopping (topping: string): void;
-}
+console.log(Cupon.create(25));
 
-class Pizza extends Sizes implements PizzaInterface {
-    public toppings: string[] = [];
-
-    constructor(readonly name: string, sizes: string[]) {
-        super(sizes);
-    }
-
-    public updateSizes (sizes: string[]) {
-        this.sizes = sizes;
-    }
-
-    public addTopping (topping: string) {
-        this.toppings.push(topping);
-    }
-}
-
-const pizza = new Pizza ('Pepperoni', ['small', 'medium']);
-
-console.log(pizza.availableSizes);
-pizza.updateSizes(['large']);
-console.log(pizza.availableSizes);
+//const date = Date.now()
